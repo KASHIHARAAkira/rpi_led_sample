2e0ec27325c4ffe32b6f00241a41fe3a4939239c
Akira Kashihara <akira.kashihara@gmail.com>
2017-07-22 07:42:31 +0000

    Add exception handling code to blink_led.py. If you insert Keyboard Interrupt event, LED is OFF.

--- blink_led.py
+++ blink_led.py
@@ -4,8 +4,12 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-while(True):
-  GPIO.output(14,GPIO.HIGH)
-  time.sleep(5)
-  GPIO.output(14,GPIO.LOW)
-  time.sleep(5)
+try:
+  while(True):
+    GPIO.output(14,GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(14,GPIO.LOW)
+    time.sleep(5)
+
+except KeyboardInterrupt:
+  GPIO.output(14, GPIO.LOW);
